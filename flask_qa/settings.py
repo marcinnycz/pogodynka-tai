1f51f938a0608fb74f84578d8ea9b0b11b4b7949
Marcin Nycz <mnycz@op.pl>
2022-01-05 16:24:00 +0100

    Fix sqlalchemy connection

--- flask_qa/settings.py
+++ flask_qa/settings.py
@@ -1,5 +1,8 @@
 import os 
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+SQLALCHEMY_DATABASE_URI = uri
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
