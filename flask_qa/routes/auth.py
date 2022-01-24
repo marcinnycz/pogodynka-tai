85d85b8a31fc010d2a62bc0a54d12d8a33ebe118
Marcin Nycz <mnycz@op.pl>
2022-01-24 23:07:09 +0100

    Stop creating admins

--- flask_qa/routes/auth.py
+++ flask_qa/routes/auth.py
@@ -33,7 +33,7 @@ def register():
         user = User(
             name=name, 
             unhashed_password=unhashed_password,
-            admin=True,  
+            admin=False,  
         )
 
         db.session.add(user)
